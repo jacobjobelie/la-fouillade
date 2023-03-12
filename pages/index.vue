238ada182ae19ef1fed02faded6c287123068157
Samuel Elie <selie@anduril.com>
2023-03-12 15:50:12 -0700

    beh

--- pages/index.vue
+++ pages/index.vue
@@ -30,20 +30,8 @@ let i = 1
 </script>
 
 <style scoped>
-.container {
-  height: 100%;
-}
 
-.welcome {
-  width: 100%;
-  margin: 0 auto;
-  text-align: center;
-  display: grid;
-  place-items: center;
-  height: calc(100vh - 92px - 70px);
-}
-
-.about {
-  box-shadow: 0 5 10 15 rgba(0, 0, 0, 0.445);
+.bg {
+  @apply mix-blend-screen
 }
 </style>
